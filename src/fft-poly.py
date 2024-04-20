30edb590e8f7f3f44eec98b8dace1437100bd6ab
Ethan Spillane <z5477667@ad.unsw.edu.au>
2024-04-20 21:00:36 +1000

    Refactor Fourier transform prediction code

--- src/fft-poly.py
+++ src/fft-poly.py
@@ -3,63 +3,72 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import sys
+import scraper
 
-symbol = sys.argv[1] # choose ticker symbol (e.g., TSLA, GOOG, AAPL)
-timeframe = sys.argv[2] # choose timeframe (e.g., max, 5y, 6mo)
-poly_degree = int(sys.argv[3]) # choose polynomial degree as basis for extrapolation (e.g., 3, 5, 10)
+symbol = "TSLA"
+poly_degree = 3
 
 # e.g. run 'python3 fft.py TSLA max 5'
 
-ticker = yf.Ticker(symbol)
-dataset_ex_df = ticker.history(period=timeframe)
-# dataset_ex_df['Close'].plot(title=f'{symbol} stock price ($)', figsize=(14,7))
+
+data = scraper.get_all_data(symbol)
 
 # Calculate the Fourier Transform
-data_FT = dataset_ex_df[['Close']]
-close_fft = np.fft.fft(np.asarray(data_FT['Close'].tolist()))
-fft_df = pd.DataFrame({'fft':close_fft})
-fft_df['absolute'] = fft_df['fft'].apply(lambda x: np.abs(x))
-fft_df['angle'] = fft_df['fft'].apply(lambda x: np.angle(x))
+def calculate_fourier_transform(data):
+    closeData = data[['Close']]
+    fft = np.fft.fft(np.asarray(closeData['Close'].tolist()))
 
-# Plot the Fourier Transforms
-transforms = []
-plt.figure(figsize=(14, 7), dpi=100)
-plt.plot(np.asarray(data_FT['Close'].tolist()),  label='Real')
-for num_ in [3, 6, 9]:
-    fft_list_m10 = np.copy(close_fft)
-    fft_list_m10[num_:-num_] = 0
-    transform = plt.plot(np.fft.ifft(fft_list_m10), label='Fourier transform with {} components'.format(num_))
-    # save the matplotlib object for later extrapolation
-    transforms.append(transform[0])
-plt.xlabel('Days')
-plt.ylabel('USD')
-plt.title(f'{symbol} (close) stock prices & Fourier transforms')
-plt.legend()
+    transforms = []
 
-colours = ['y', 'g', 'r']
+    for n in [3, 6, 9]:
+        fft_intermediete = np.copy(fft)
+        fft_intermediete[n:-n] = 0
+        transform = plt.plot(np.fft.ifft(fft_intermediete), label='Fourier transform with {} components'.format(n))
+        # save the matplotlib object for later extrapolation
+        transforms.append(transform[0])
+    return transforms
+def generate_polynomial_fit(data):
+    return plt
+def calculate_polynomial_fit(transforms, poly_degree):
+    # Extrapolate predicted polynomials on the Fourier Transforms
+    polynomials = []
 
-# Extrapolate predicted polynomials on the Fourier Transforms
-colour_index = 0
-for transform in transforms:
-    x_vals = transform.get_data()[0]
-    y_vals = transform.get_data()[1]
-    z = np.polyfit(x_vals, y_vals, poly_degree)
-    f = np.poly1d(z)
+    for transform in transforms:
+        x_vals = transform.get_data()[0]
+        y_vals = transform.get_data()[1]
+        z = np.polyfit(x_vals, y_vals, poly_degree)
+        f = np.poly1d(z)
+        polynomials.append(f)
+    return polynomials
 
-    # plots points of the extrapolated polynomial
-    # first arg is starting x-coordinate
-    # second arg is ending x-coordinate
-    # third arg is number of points
-    # change these values according to the generated width of the original plot
-    # e.g., if command line args are 'TSLA', 'max', and 5 then
-    # 3000, 3600, 50 means show the extrapolated polynomial between 3000 and 3600
-    # with 50 points (predicting next 100 days)
-    for x1 in np.linspace(3000, 3600, 50):
-        plt.plot(x1, f(x1), colours[colour_index] + '+')
-    
-    colour_index += 1
+        # plots points of the extrapolated polynomial
+        # first arg is starting x-coordinate
+        # second arg is ending x-coordinate
+        # third arg is number of points
+        # change these values according to the generated width of the original plot
+        # e.g., if command line args are 'TSLA', 'max', and 5 then
+        # 3000, 3600, 50 means show the extrapolated polynomial between 3000 and 3600
+        # with 50 points (predicting next 100 days)
+def plot_polynomial(polynomials, start, extrapolation):
+    colour_index = 0
+    colours = ['y', 'g', 'r']
+    for f in polynomials:
+        for x1 in np.linspace(start, start + extrapolation, 100):
+            plt.plot(x1, f(x1), colours[colour_index] + '+')
 
-plt.show()
+        colour_index += 1
+
+def polynomial_predict(f, day):
+    return f(day) - f(day - 1)
+
+def show():
+    plt.style.use('ggplot')
+    plt.xlabel('Days')
+    plt.ylabel('USD')
+    plt.ylim(0)
+    plt.title(f'{symbol} (close) stock prices & Fourier transforms')
+    plt.legend()
+    plt.show()
 
 # notes:
 # 
@@ -70,3 +79,15 @@ plt.show()
 #   or interpolation done here <https://www.math.utah.edu/~gustafso/s2018/2270/projects-2016/williamsBarrett/williamsBarrett-Fast-Fourier-Transform-Predicting-Financial-Securities-Prices.pdf>
 #   essentially meaning the model from the first source (which is used here to generate the three fourier transforms)
 #   is incompatible with what is being suggested in the other two sources
+
+if __name__ == '__main__':
+    testdata = scraper.test_extrapolationToday(symbol,200, 60)
+    data = scraper.get_last(symbol, 365)
+    plt.figure(figsize=(14, 7), dpi=100)
+    plt.plot(np.asarray(testdata[1]['Close'].tolist()),  label='Real')
+    transforms = calculate_fourier_transform(testdata[0])
+    polynomials = calculate_polynomial_fit(transforms, poly_degree)
+    plot_polynomial(polynomials, 0, len(data))
+
+
+    show()
--- src/scraper.py
+++ src/scraper.py
@@ -1,14 +1,26 @@
 import yfinance as yf
 import datetime
 import math
+
+class Model:
+    def __init__(self, data, start, end):
+        self.data = data
+        self.start = start
+        self.end = end
+    def get_day_num(self, day):
+        return (day - self.start).days
+
 def get_all_data(symbol):
     ticker = yf.Ticker(symbol)
     return ticker.history(period='max')
 
 def get_data(symbol, start, end):
     return yf.download(symbol, start=start, end=end)
+
+def get_last(symbol, days):
+    return yf.download(symbol, start=datetime.date.today() - datetime.timedelta(days=days), end=datetime.date.today())
 def test_extrapolation(symbol, start, end, num_days):
-    if (start > end - num_days):
+    if (start > end - datetime.timedelta(num_days)):
         return 0;
     else:
         completeData = yf.download(symbol, start=start, end=end)
